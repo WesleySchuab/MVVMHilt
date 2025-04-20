2eaf0a299dcfc16fe7658efcf6c44bc716b926ec
Wesley Schuab <yinnihisoka@gmail.com>
2025-04-20 09:48:24 -0300

    showing the data

--- app/src/main/java/com/kaisa/mvvmhilt/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/kaisa/mvvmhilt/presentation/ui/MainActivity.kt
@@ -5,19 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.kaisa.mvvmhilt.R
+import com.kaisa.mvvmhilt.databinding.ActivityMainBinding
+import com.kaisa.mvvmhilt.presentation.viewmodel.UsuarioViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var usuarioViewModel: UsuarioViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        usuarioViewModel = ViewModelProvider(this)[UsuarioViewModel::class.java]
+        usuarioViewModel.usuario.observe(this){ listaUsuarios ->
+            var listaResultado =""
+            listaUsuarios.forEach { usuario ->
+                val nome = usuario.nome
+                val sobrenome = usuario.sobrenome
+                val idade = usuario.idade
+                listaResultado += " +)$nome - $sobrenome - $idade"
+            }
+
+        binding.textResultado.text = listaResultado
+        }
+
     }
 }
--- app/src/main/java/com/kaisa/mvvmhilt/presentation/viewmodel/UsuarioViewModel.kt
+++ app/src/main/java/com/kaisa/mvvmhilt/presentation/viewmodel/UsuarioViewModel.kt
@@ -1,8 +1,13 @@
 package com.kaisa.mvvmhilt.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kaisa.mvvmhilt.domain.model.Usuario
 import com.kaisa.mvvmhilt.domain.usecase.GetUsuariosUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -10,4 +15,24 @@ import javax.inject.Inject
 class UsuarioViewModel @Inject constructor(
     private val usuarioUseCase: GetUsuariosUseCase
 ): ViewModel() {
+
+    //Privado
+    private val _usuarios = MutableLiveData<List<Usuario>>()
+
+    //Publico que não pode ser alterado mas pode ser visualizado
+    val usuario: LiveData<List<Usuario>>
+    get() = _usuarios
+    //Inicia a classe Poderia ser um botão
+    // Toda vez que for criado uma instancia de Viewmodel ele chama a função recuperarUsuarios
+    init {
+        recuperarUsuarios()
+    }
+
+    fun recuperarUsuarios(){
+        viewModelScope.launch {
+           val listaUsuarios = usuarioUseCase()
+            _usuarios.postValue (listaUsuarios)
+        }
+    }
+
 }
